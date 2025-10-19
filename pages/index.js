be55a628eb126f00521929a022547c789de7441b
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-19 17:14:55 +0330

    Running Server-side Code & Using the Filesystem

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
+import fs from "fs/promises";
+import path from "path";
+
 function HomePage(props) {
-  const {products} = props
+  const { products } = props;
   return (
     <ul>
-      {products.map(product=> <li key={product.id}>{product.title}</li>)}
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 }
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
   return {
     props: {
-      products: [{ id: "p1", title: "product 1" }],
+      products: data.products,
     },
   };
 }
