79b23364441fd4b47f8267a93c55717f7c38a1a6
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-21 15:17:53 +0330

    Fallback Pages & Not Found Pages

--- pages/[pid].js
+++ pages/[pid].js
@@ -1,3 +1,4 @@
+
 import fs from "fs/promises";
 import path from "path";
 
@@ -20,6 +21,7 @@ async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
   return data;
 }
 
@@ -27,8 +29,10 @@ export async function getStaticProps(context) {
   const { params } = context;
   const productId = params.pid;
   const data = await getData();
-
   const product = data.products.find((product) => product.id === productId);
+  if(!product){
+    return {notFound : true}
+  }
 
   return {
     props: {
@@ -38,8 +42,9 @@ export async function getStaticProps(context) {
 }
 export async function getStaticPaths() {
   const data = await getData();
-  const ids = data.map((product) => product.id);
-  const pathsWithParams = ids.map((id) => ({ params: { ide: id } }));
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+  console.log(pathsWithParams);
   
   return {
     paths: pathsWithParams ,
