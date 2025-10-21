16271d04446d36304c641f1fdb52c5576883ff3c
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-21 16:23:20 +0330

    Using getServerSideProps for Server-side Rendering

---
+++ pages/user-profile.js
@@ -0,0 +1,10 @@
+export default function UserProfilePage(props) {
+  return <h1>{props.username}</h1>;
+}
+export async function getServerSideProps() {
+  return {
+    props: {
+      username: "max",
+    },
+  };
+}
