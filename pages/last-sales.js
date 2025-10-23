115e47a3218015a573b07c2867fc47fad22fdf54
salehTorkashvand <saleh.tarkashvand@gmail.com>
2025-10-23 20:19:26 +0330

    Using the useSWR NextJS Hook

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,37 +1,46 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function LastSalesPage() {
-  const [sales, setSales] = useState();
-  const [loading, isLoading] = useState(false);
+  const [sales, setSales] = useState([]);
+  const { data, error, isLoading } = useSWR(
+    "https://nextjs-course-b6a67-default-rtdb.firebaseio.com/sales.json",
+    fetcher
+  );
+
   useEffect(() => {
-    isLoading(true);
-    fetch("https://nextjs-course-b6a67-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        isLoading(false);
-      });
-  }, []);
-  if (loading) {
-    return <p>loading ...</p>;
+    if (data && typeof data === "object") {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
   }
-  if (!sales) {
-    return <p>no sales yet</p>;
+
+  if (error) {
+    return <p>Failed to fetch data</p>;
   }
+
+  if (!sales.length) {
+    return <p>No data yet</p>;
+  }
+
   return (
     <ul>
       {sales.map((sale) => (
         <li key={sale.id}>
-          {sale.username}-${sale.volume}
+          {sale.username} - ${sale.volume}
         </li>
       ))}
     </ul>
